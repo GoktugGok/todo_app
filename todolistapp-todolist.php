3babc88e822c92ef23bd2bcaf03cce30188844cc
Göktuğ GÖK <gokgoktug0@gmail.com>
2022-12-15 11:31:45 +0300

    first commit

---
+++ todolistapp-todolist.php
@@ -0,0 +1,24 @@
+<?php
+/*
+
+*Klasör ve Routing Yapısı
+*Tema Entegrasyonu
+*Veritabanı Bağlantısı
+*Veritabanı Tabloları
+*Login İşlemleri
+
+*Todo Kategorileri
+    ?listeleme
+    ?Ekleme
+    ?Güncelleme
+    ?Silme
+
+*Todo İşlemleri
+    ?listeleme
+    ?Ekleme
+    ?Güncelleme
+    ?Silme
+
+*Todo İstatistikleri
+ 
+*/
