4ea189bd586ac3f37a08df1ea308c6b8a33c6d73
Göktuğ GÖK <gokgoktug0@gmail.com>
2022-12-17 19:09:26 +0300

    login-index

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+const BASEDIR = '/opt/lampp/htdocs/çalışmalar/TodoApp';
+const URL = 'http://localhost/çalışmalar/TodoApp/';
+const DEV_MODE = true;
---
+++ language/tr.php
@@ -0,0 +1,8 @@
+<?php
+$lang =[
+    'title' => 'Başlık',
+    'Oturum Açın' => 'Oturum Açın',
+    'E-Posta' => 'E-Posta',
+    'Şifreniz' => 'Şifreniz',
+    'Giriş Yap' => 'Giriş Yap',
+];
