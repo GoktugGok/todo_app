bf50661f91670c5543cfb23ac6d2d46047376f59
Göktuğ GÖK <gokgoktug0@gmail.com>
2022-12-25 13:22:49 +0300

    SweetAlert-Axios

--- View/static/header.php
+++ View/static/header.php
@@ -9,7 +9,6 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="<?= assets('plugins/fontawesome-free/css/all.min.css')?>">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?= assets('css/adminlte.min.css')?>">
 </head>
